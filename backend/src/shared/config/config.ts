09e44ab8f04b727452c6189365ba69b7d78bb97f
Emon Hossain <efadhemon@gmail.com>
2023-01-22 14:54:49 +0600

    vercel deploy issue fix

--- backend/src/shared/config/config.ts
+++ backend/src/shared/config/config.ts
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
 const config = {
-    bd: {
-        uri: process.env.DB_URI || "mongodb://localhost:5000/",
-    },
-    server: {
-        port: process.env.PORT || 5000,
-    },
+  db: {
+    uri: process.env.DB_URI || "mongodb://localhost:5000/",
+  },
+  server: {
+    port: process.env.PORT || 5000,
+  },
 };
 
 export default config;
